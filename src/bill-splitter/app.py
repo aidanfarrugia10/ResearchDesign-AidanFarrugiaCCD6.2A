25a40b397c399717a885ffd548ff7fd18fc570bd
aid-farr <aidanfarrugia2004@gmail.com>
2025-05-07 15:08:02 +0200

    Base Foundation of uploading Receipt and storing inside folder

--- src/bill-splitter/app.py
+++ src/bill-splitter/app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello Test"
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return 'File successfully uploaded'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
